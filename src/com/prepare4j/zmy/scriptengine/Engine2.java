ffeefc2d902afdefac7954471eaf0a48ca8595ea
605113068@qq.com <448676zmy>
2019-03-08 19:19:54 +0800

    完成Engine1、2、3

--- src/com/prepare4j/zmy/scriptengine/Engine2.java
+++ src/com/prepare4j/zmy/scriptengine/Engine2.java
@@ -7,7 +7,18 @@ import javax.script.ScriptException;
 import static java.lang.System.err;
 import static java.lang.System.out;
 
-public class Engine1 {
+/**
+ * function myFunction(applyMoney)
+ * {
+ *     var output ='' ;
+ * 	output = applyMoney;
+ * 	alert(output)
+ * }
+ */
+
+
+//http://www.runoob.com/try/try.php?filename=tryjs_function_xx
+public class Engine2 {
     public static void main(String args[]) {
         ScriptEngineManager manager = new ScriptEngineManager();
         // 建立javascript脚本引擎
@@ -15,10 +26,16 @@ public class Engine1 {
         try {
             // 将变量name和变量值abcdefg传给javascript脚本
 
-            engine.put("applyMoney",  500);
+            engine.put("applyMoney",  80);
+            engine.put("maxMoney",  100);
             // 开始执行脚本
 
-            engine.eval("var output =0;output = applyMoney;");
+            engine.eval("var output = 0;" +
+                    "if(applyMoney >= maxMoney){" +
+                    "output = maxMoney;" +
+                    "}else{" +
+                    "output = applyMoney;" +
+                    "}");
             // 得到output变量的值
 
             int money = Integer.valueOf(engine.get("output").toString());
--- src/com/prepare4j/zmy/scriptengine/Engine3.java
+++ src/com/prepare4j/zmy/scriptengine/Engine3.java
@@ -4,10 +4,23 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
+import java.math.BigDecimal;
+
 import static java.lang.System.err;
 import static java.lang.System.out;
 
-public class Engine1 {
+/**
+ * function myFunction(applyMoney)
+ * {
+ *     var output ='' ;
+ * 	output = applyMoney;
+ * 	alert(output)
+ * }
+ */
+
+
+//http://www.runoob.com/try/try.php?filename=tryjs_function_xx
+public class Engine3 {
     public static void main(String args[]) {
         ScriptEngineManager manager = new ScriptEngineManager();
         // 建立javascript脚本引擎
@@ -15,14 +28,15 @@ public class Engine1 {
         try {
             // 将变量name和变量值abcdefg传给javascript脚本
 
-            engine.put("applyMoney",  500);
+            engine.put("applyMoney",  50);
+            engine.put("ratio",  0.6d);
+            engine.put("maxMoney",  100);
             // 开始执行脚本
 
-            engine.eval("var output =0;output = applyMoney;");
+            engine.eval("var output = applyMoney * ratio;");
             // 得到output变量的值
 
-            int money = Integer.valueOf(engine.get("output").toString());
-            out.println("应报金额："+ money);
+            out.println("应报金额："+ engine.get("output"));
         } catch (ScriptException e) {
             err.println(e);
         }
