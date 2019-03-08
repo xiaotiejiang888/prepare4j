fc73e4b655ba31663c1176e608fb84adabcd0a23
605113068@qq.com <448676zmy>
2019-03-08 18:42:51 +0800

    完成Engine1

--- src/com/prepare4j/zmy/scriptengine/Engine1.java
+++ src/com/prepare4j/zmy/scriptengine/Engine1.java
@@ -7,7 +7,7 @@ import javax.script.ScriptException;
 import static java.lang.System.err;
 import static java.lang.System.out;
 
-public class ReverseString2 {
+public class Engine1 {
     public static void main(String args[]) {
         ScriptEngineManager manager = new ScriptEngineManager();
         // 建立javascript脚本引擎
@@ -15,16 +15,14 @@ public class ReverseString2 {
         try {
             // 将变量name和变量值abcdefg传给javascript脚本
 
-            engine.put("name",  "zmy");
+            engine.put("applyMoney",  500);
             // 开始执行脚本
 
-            engine.eval("var output ='' ;" +
-                    "for (i = 0; i <= name.length; i++) {" +
-                    " output = name.charAt(i) + output" +
-                    "}");
+            engine.eval("var output =0 ;" +
+                    "output = applyMoney;");
             // 得到output变量的值
 
-            String name = (String) engine.get("output");
+            int name = Integer.valueOf(engine.get("output").toString());
             out.printf("被翻转后的字符串：%s", name);
         } catch (ScriptException e) {
             err.println(e);
