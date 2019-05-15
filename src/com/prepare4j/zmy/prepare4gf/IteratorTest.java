4752b418f47d86c35d775589c331b6aecb5fb561
xiaotiejiang888 <605113068@qq.com>
2019-05-15 23:23:28 +0800

    规范准备的代码

--- src/com/prepare4j/zmy/prepare4gf/IteratorTest.java
+++ src/com/prepare4j/zmy/prepare4gf/IteratorTest.java
@@ -9,23 +9,30 @@ public class IteratorTest {
         List<String> list = new ArrayList<>();
         list.add("1");
         list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
+//        removeByIterator(list,"2");
+        removeByForeach(list,"4");
+    }
+
+    private static void removeByIterator(List<String> list,String i) {
         Iterator<String> iterator = list.iterator();
         while (iterator.hasNext()) {
             String item = iterator.next();
-            if ("2".equals(item)) {
+            if (i.equals(item)) {
                 iterator.remove();
             }
         }
         System.out.println(list.toString());
     }
-//    public static void main(String[] args) {
-//        List<String> list = new ArrayList<>();
-//        list.add("1");
-//        list.add("2");
-//        for (String item : list) {
-//                list.remove(item);
-//            }
-//        }
-//        System.out.println(list.toString());
-//    }
+
+    private static void removeByForeach(List<String> list,String i) {
+        for (String item : list) {
+            if (i.equals(item)) {
+                list.remove(item);
+            }
+        }
+        System.out.println(list.toString());
+    }
 }
